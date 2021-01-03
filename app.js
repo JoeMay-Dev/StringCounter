b6f9c35c6aba24ad144c2b67977f8ba1a816ab50
Joe May <jmmay1974@gmail.com>
2021-01-02 19:20:41 -0700

    updated index.html & app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const cstring = document.getElementById('cstring');
+const countBtn = document.getElementById('count');
 
+function countChars(input) {
+    return input.length();
+}
+
+// console.log(countChars(cstring));
+
+countBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log(countChars(cstring));
+});
