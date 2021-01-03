35f997f08d7dc8d6de5e5054f9fe307b61315624
Joe May <jmmay1974@gmail.com>
2021-01-02 20:06:23 -0700

    updated index.html & app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const form = document.getElementById('form');
 const cstring = document.getElementById('cstring');
 const countBtn = document.getElementById('count');
+const resetBtn = document.getElementById('clear');
 
 function countChars(input) {
-    return input.length();
+   return input.value.length;
+}
+
+function resetPg(){
+    form.reset();
 }
 
 // console.log(countChars(cstring));
@@ -11,3 +17,5 @@ countBtn.addEventListener('click', function(e){
     e.preventDefault();
     console.log(countChars(cstring));
 });
+
+resetBtn.addEventListener('click', resetPg());
